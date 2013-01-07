514ba9110894af2086ea8b95fac5db845ee14576
Matías Iturburu <matias.iturburu@tallertechnologies.com>
2013-01-07 12:35:40 -0300

    This way it doesn't complains about not having a database setting

--- ping/settings.py
+++ ping/settings.py
@@ -12,9 +12,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-
 #DATABASE_URL = 'sqlite:///{}'.format(os.path.join(BASE_DIR,'partidos.db')
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
