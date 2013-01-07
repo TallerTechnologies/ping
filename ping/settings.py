962ba986c8b9c5de79d5545f492b09c664377655
Matías Iturburu <matias.iturburu@tallertechnologies.com>
2013-01-07 12:13:06 -0300

    Heroky integration.
    It is now required that an envirnoment variable DATABASE_URL be set, ie:
    
    DATABASE_URL='project.db'

--- ping/settings.py
+++ ping/settings.py
@@ -1,4 +1,6 @@
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(__file__)
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,17 +12,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR,'partidos.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
+#DATABASE_URL = 'sqlite:///{}'.format(os.path.join(BASE_DIR,'partidos.db')
+DATABASES['default'] =  dj_database_url.config()
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
