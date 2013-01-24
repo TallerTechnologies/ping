9d72e2f6c16cb110c1635a1ddfbc1c92e5adf4b9
Matías Iturburu <matias.iturburu@tallertechnologies.com>
2013-01-24 10:22:04 -0300

    logout view

--- ping/urls.py
+++ ping/urls.py
@@ -15,4 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     (r'^openid/', include('django_openid_auth.urls')),
+    (r'logout/', 'django.contrib.auth.views.logout',  {'next_page':'/'} )
 )
